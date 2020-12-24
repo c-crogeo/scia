59a54085392e92c7142c6a4a236ed8b1d6267334
crogeo <c.crogeo@gmail.com>
2020-12-24 22:58:19 +0100

    version 1.0.1

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
 
+with open('README.md', encoding='utf-8') as fp:
+    long_description = fp.read()
+
+
 setup(
     name='scia',
-    version='1.0.0',
+    version='1.0.1',
     packages=['scia', 'scia.handlers'],
     url='https://github.com/crogeo/scia',
     license='MIT License',
     author='crogeo.org',
     author_email='',
-    description='Python logging extension'
+    description='Python logging extension',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
